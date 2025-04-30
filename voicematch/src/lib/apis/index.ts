dc3ea558337f3c26f965ec9516edac3b1835706b
HilloriDesai <hilloridesai@gmail.com>
2025-04-30 22:05:04 +0530

    fix: typo

--- voicematch/src/lib/apis/index.ts
+++ voicematch/src/lib/apis/index.ts
@@ -1,9 +1,9 @@
 export * from "./createActor";
 export * from "./createJob";
-export * from "./getVoiceActorByid";
 export * from "./getVoiceActors";
 export * from "./uploadScript";
 export * from "./getActorReviews";
+export * from "./getVoiceActorById";
 
 export interface ValidationResult<T = unknown> {
   success: boolean;
